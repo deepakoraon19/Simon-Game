c89e8dda693b5b226166e112d0087498b40bb362
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-06-29 13:28:47 +0530

    Restart Feature and score count

--- index.js
+++ index.js
@@ -1,74 +1,77 @@
-let arr=[];
+let arr = [];
 const btn = document.querySelectorAll(".btn");
 
-let random = Math.floor(Math.random()*(4-0)+0);
-let output=[];
+let random = Math.floor(Math.random() * (4 - 0) + 0);
+let output = [];
 let glow;
 let game = false;
+let heading = document.querySelector("#level-title");
+let rules = document.querySelector("p");
 
-function glowButton(glow){
-    console.log("jalao");
-    btn[glow].style.border="10px dashed black";
-    setTimeout(()=>{
-        btn[glow].style.border="10px solid black";
-    },1000);
+function glowButton(glow) {
+  console.log("jalao");
+  btn[glow].classList.add("glow");
+  setTimeout(() => {
+    btn[glow].classList.remove("glow");
+  }, 1000);
 }
 
-function newGlow()
-{
-    arr.push(Math.floor(Math.random()*(4-0)+0));
-    glow=arr[arr.length-1];
-    glowButton(glow);
-    output=[];
+function newGlow() {
+  arr.push(Math.floor(Math.random() * (4 - 0) + 0));
+  glow = arr[arr.length - 1];
+  glowButton(glow);
+  output = [];
 }
 
-for(let i=0;i<btn.length;i++)
-{
-    btn[i].addEventListener('click',()=>{
-        if(arr.length==0)
-        {
-            arr.push(i);
-            console.log("first");
-            arr.push(Math.floor(Math.random()*(4-0)+0));
-            glow=arr[arr.length-1];
-            glowButton(glow);
-            game=true;
+document.addEventListener("keypress", (e) => {
+  if ((e.code = "Space")) {
+    arr = [];
+    output = [];
+    game = true;
+    // run()
+    heading.innerHTML = "Restarted";
+    heading.style.animationName = "example";
+    heading.style.animationDuration = "3s";
+
+    setTimeout(() => {
+      heading.innerHTML = "Press Any Key to Start";
+      heading.style.animationName = "";
+      heading.style.animationDuration = "";
+    }, 700);
+  }
+});
+
+let run = () => {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      if (arr.length == 0) {
+        arr.push(i);
+        arr.push(Math.floor(Math.random() * (4 - 0) + 0));
+        glow = arr[arr.length - 1];
+        glowButton(glow);
+        game = true;
+      } else if (arr.length >= output.length) {
+        output.push(i);
+        console.log(arr, output);
+
+        for (let j = 0; j < output.length; j++) {
+          if (arr[j] != output[j]) {
+            console.log("galat" + arr[j], output[j]);
+            game = false;
+            heading.innerHTML = "You Lost Press SPACE to restart";
+          }
         }
-        else if(arr.length>=output.length)
-        {
-            
-            output.push(i);
-            console.log(arr,output);
-            
-            for(let j=0;j<output.length;j++)
-            {
-                if(arr[j]!=output[j])
-                {
-                    console.log("galat"+arr[j], output[j]);
-                    game=false;
-                }
-            }
-            if(game==true&&arr.length==output.length)
-            {
-                console.log("naya jalao");
-                setTimeout(newGlow(),2000);
-            }
-            // console.log("out");            
+        if (game == true && arr.length == output.length) {
+          console.log("naya jalao");
+          heading.innerHTML = `Score : ${arr.length}`;
+          setTimeout(newGlow(), 2000);
         }
-        else if(game){
-            
-            
-        }
-        
-        
-        // console.log(arr);
-        //console.log(Math.floor(Math.random()*(4-0))+0);
-    })
-}
+        // console.log("out");
+      }
+      // console.log(arr);
+      //console.log(Math.floor(Math.random()*(4-0))+0);
+    });
+  }
+};
 
-
-
-
-
-
-
+run();
